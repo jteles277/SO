056ca59b55e3374484c07a15312e165e1b426360
jteles277 <59253209+jteles277@users.noreply.github.com>
2021-11-25 02:48:54 +0000

    commented

--- NetStat.sh
+++ NetStat.sh
@@ -2,24 +2,25 @@
 
 # functions
   SortRx() {
+    # basic selection sort  
     for (( i = 1; $i < $(($N-1)); $((i = $i + 1)) ))
     do
 	    for (( e = $(($i+1)); $e < $N; $((e = $e + 1)) ))
 	    do
 
-        rx_i=$(echo "${data[$i]}" | awk '{print $3;}');
-        rx_e=$(echo "${data[$e]}" | awk '{print $3;}'); 
+            rx_i=$(echo "${data[$i]}" | awk '{print $3;}');
+            rx_e=$(echo "${data[$e]}" | awk '{print $3;}'); 
 
-		    if [[ $rx_e -gt $rx_i ]]
-        then 
+            if [[ $rx_e -gt $rx_i ]]
+            then 
 
-          
-          temp=${data[$e]};
-          data[$e]=${data[$i]};
-          data[$i]=$temp;
+            
+            temp=${data[$e]};
+            data[$e]=${data[$i]};
+            data[$i]=$temp;
 
-             
-		    fi
+                
+            fi
 	    done
     done
   }
@@ -28,7 +29,7 @@
 # main
 
 
-    # get inicial array 
+    # get inicial array and organize it in: i_data[] = interface rx tx
     IFS=$'\n' read -r -d '' -a interfaces < <( ifconfig -a | grep ": " | awk '{print $1}' | tr -d : && printf '\0' )
     IFS=$'\n' read -r -d '' -a i_RXs < <( ifconfig -a | grep "RX packets" | awk '{print $5}' | tr -d : && printf '\0' )  
     IFS=$'\n' read -r -d '' -a f_TXs < <( ifconfig -a | grep "TX packets" | awk '{print $5}' | tr -d : && printf '\0' )
@@ -38,10 +39,10 @@
         i_data[$i]="${interfaces[$i]} ${i_RXs[$i]} ${f_TXs[$i]}";
     done    
     
-    # espera nºsegundos recebido
+    # wait given seconds
     sleep $1
 
-    # get final array 
+    # get final array and organize it in: i_data[] = interface rx tx
     IFS=$'\n' read -r -d '' -a interfaces < <( ifconfig -a | grep ": " | awk '{print $1}' | tr -d : && printf '\0' )
     IFS=$'\n' read -r -d '' -a i_RXs < <( ifconfig -a | grep "RX packets" | awk '{print $5}' | tr -d : && printf '\0' )  
     IFS=$'\n' read -r -d '' -a f_TXs < <( ifconfig -a | grep "TX packets" | awk '{print $5}' | tr -d : && printf '\0' )
@@ -49,28 +50,39 @@
     do
         data[$i]="${interfaces[$i]} ${i_RXs[$i]} ${f_TXs[$i]}";
     done    
+    
 
 
+
+    #get
     for ((i=1; i < $N; i++ ))
-    do
+    do  
+        # get innterface
         interface=$(echo "${i_data[$i]}" | awk '{print $1;}');
 
+        # rx/tx = get rx/tx from array  |  dif = rFinal - rInicial  |  rate  = dif/#secs
+
+        # r
         i_rx=$(echo "${i_data[$i]}" | awk '{print $2;}');
         rx=$(echo "${data[$i]}" | awk '{print $2;}');
-        r_Dif=($(($rx-$i_rx))); # r
+        r_Dif=($(($rx-$i_rx))); 
         r_Rate=($(($r_Dif/$1)));
 
+        # t
         i_tx=$(echo "${i_data[$i]}" | awk '{print $3;}');
         tx=$(echo "${data[$i]}" | awk '{print $3;}');
-        t_Dif=($(($tx-$i_tx))); # r
+        t_Dif=($(($tx-$i_tx))); 
         t_Rate=($(($t_Dif/$1)));
 
+        # return changed and added values to the array  
         data[$i]="$interface $t_Dif $r_Dif $t_Rate $r_Rate";
         
     done
 
+    # Base Sort
     SortRx
 
+    #print 
     printf "%9s %9s %9s %9s %9s\n" "NETIF" "TX" "RX" "TRATE" "RRATE"; # print
     for (( i=1; i < $N; i++ )); do
         int=$(echo "${data[$i]}" | awk '{print $1;}');
