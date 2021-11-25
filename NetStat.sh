94322e5e1467d46e048d08474a8f8e49e5aaa180
jteles277 <59253209+jteles277@users.noreply.github.com>
2021-11-25 02:41:30 +0000

    Working Base stage

--- NetStat.sh
+++ NetStat.sh
@@ -28,48 +28,59 @@
 # main
 
 
-  # get inicial array 
-  IFS=$'\n' read -r -d '' -a i_data < <( ifconfig -a -s | awk 'BEGIN{OFS ="\t"}; {print $1, $3, $7;}' )  
-  N=${#i_data[@]}; 
-  
-  sleep $1
-
-  # get final array
-  IFS=$'\n' read -r -d '' -a data < <( ifconfig -a -s | awk 'BEGIN{OFS ="\t"}; {print $1, $3, $7;}' )  
-
-
-
-
-  for ((i=1; i < $N; i++ ))
-  do
-    interface=$(echo "${i_data[$i]}" | awk '{print $1;}');
-
-    i_rx=$(echo "${i_data[$i]}" | awk '{print $2;}');
-    rx=$(echo "${data[$i]}" | awk '{print $2;}');
-    r_Dif=($(($rx-$i_rx))); # r
-    r_Rate=($(($r_Dif/$1)));
-
-    i_tx=$(echo "${i_data[$i]}" | awk '{print $3;}');
-    tx=$(echo "${data[$i]}" | awk '{print $3;}');
-    t_Dif=($(($tx-$i_tx))); # r
-    t_Rate=($(($t_Dif/$1)));
-
-    data[$i]="$interface $t_Dif $r_Dif $t_Rate $r_Rate";
+    # get inicial array 
+    IFS=$'\n' read -r -d '' -a interfaces < <( ifconfig -a | grep ": " | awk '{print $1}' | tr -d : && printf '\0' )
+    IFS=$'\n' read -r -d '' -a i_RXs < <( ifconfig -a | grep "RX packets" | awk '{print $5}' | tr -d : && printf '\0' )  
+    IFS=$'\n' read -r -d '' -a f_TXs < <( ifconfig -a | grep "TX packets" | awk '{print $5}' | tr -d : && printf '\0' )
+    N=${#interfaces[@]};
+    for ((i=1; i < $N; i++ ))
+    do
+        i_data[$i]="${interfaces[$i]} ${i_RXs[$i]} ${f_TXs[$i]}";
+    done    
     
-  done
+    # espera nºsegundos recebido
+    sleep $1
 
-  SortRx
+    # get final array 
+    IFS=$'\n' read -r -d '' -a interfaces < <( ifconfig -a | grep ": " | awk '{print $1}' | tr -d : && printf '\0' )
+    IFS=$'\n' read -r -d '' -a i_RXs < <( ifconfig -a | grep "RX packets" | awk '{print $5}' | tr -d : && printf '\0' )  
+    IFS=$'\n' read -r -d '' -a f_TXs < <( ifconfig -a | grep "TX packets" | awk '{print $5}' | tr -d : && printf '\0' )
+    for ((i=1; i < $N; i++ ))
+    do
+        data[$i]="${interfaces[$i]} ${i_RXs[$i]} ${f_TXs[$i]}";
+    done    
 
-  printf "%9s %9s %9s %9s %9s\n" "NETIF" "TX" "RX" "TRATE" "RRATE"; # print
-  for (( i=1; i < $N; i++ )); do
-    int=$(echo "${data[$i]}" | awk '{print $1;}');
-    tx=$(echo "${data[$i]}" | awk '{print $2;}');
-    rx=$(echo "${data[$i]}" | awk '{print $3;}');
-    t_rate=$(echo "${data[$i]}" | awk '{print $4;}');
-    r_rate=$(echo "${data[$i]}" | awk '{print $5;}');
+
+    for ((i=1; i < $N; i++ ))
+    do
+        interface=$(echo "${i_data[$i]}" | awk '{print $1;}');
+
+        i_rx=$(echo "${i_data[$i]}" | awk '{print $2;}');
+        rx=$(echo "${data[$i]}" | awk '{print $2;}');
+        r_Dif=($(($rx-$i_rx))); # r
+        r_Rate=($(($r_Dif/$1)));
+
+        i_tx=$(echo "${i_data[$i]}" | awk '{print $3;}');
+        tx=$(echo "${data[$i]}" | awk '{print $3;}');
+        t_Dif=($(($tx-$i_tx))); # r
+        t_Rate=($(($t_Dif/$1)));
+
+        data[$i]="$interface $t_Dif $r_Dif $t_Rate $r_Rate";
+        
+    done
+
+    SortRx
+
+    printf "%9s %9s %9s %9s %9s\n" "NETIF" "TX" "RX" "TRATE" "RRATE"; # print
+    for (( i=1; i < $N; i++ )); do
+        int=$(echo "${data[$i]}" | awk '{print $1;}');
+        tx=$(echo "${data[$i]}" | awk '{print $2;}');
+        rx=$(echo "${data[$i]}" | awk '{print $3;}');
+        t_rate=$(echo "${data[$i]}" | awk '{print $4;}');
+        r_rate=$(echo "${data[$i]}" | awk '{print $5;}');
 
 
-    printf "%9s %9s %9s %9s %9s\n" $int $tx $rx $t_rate $r_rate;
-  done
+        printf "%9s %9s %9s %9s %9s\n" $int $tx $rx $t_rate $r_rate;
+    done
 
 #
