2d23d99e1bbb5a2b90376d1d8505b4b54bc6a950
jteles277 <59253209+jteles277@users.noreply.github.com>
2021-11-25 06:50:22 +0000

    looping(almost done)

--- NetStat.sh
+++ NetStat.sh
@@ -68,6 +68,8 @@
     order="alpha";
     max=10;
     getMax=0;
+    loop=0;
+    looping=1;
 
     # process options
     for op in "$@"; do
@@ -79,6 +81,9 @@
         fi 
 
         case $op in
+            -l)
+                loop=1;
+                ;;
             -p)
                 getMax=1;
                 ;;
@@ -106,93 +111,102 @@
         esac
     done
 
-    echo "byte_div = $byte_div";
-    echo "order = $order";
-    echo "reversed = $reversed";
-
-
-    # get inicial array and organize it in: i_data[] = interface_name rx tx
-    IFS=$'\n' read -r -d '' -a interfaces < <( ifconfig -a | grep ": " | awk '{print $1}' | tr -d : && printf '\0' )
-    IFS=$'\n' read -r -d '' -a i_RXs < <( ifconfig -a | grep "RX packets" | awk '{print $5}' | tr -d : && printf '\0' )  
-    IFS=$'\n' read -r -d '' -a f_TXs < <( ifconfig -a | grep "TX packets" | awk '{print $5}' | tr -d : && printf '\0' )
-    N=${#interfaces[@]};
-    for ((i=1; i < $N; i++ ))
-    do
-        i_data[$i]="${interfaces[$i]} ${i_RXs[$i]} ${f_TXs[$i]}";
-        echo ${i_data[$i]};
-    done    
+    #echo "info = $info";
     
-    # wait given seconds
-    sleep $sleep_time
+    #interation
+    iter=0;
 
-    # get final array and organize it in: i_data[] = interface_name rx tx
-    IFS=$'\n' read -r -d '' -a interfaces < <( ifconfig -a | grep ": " | awk '{print $1}' | tr -d : && printf '\0' )
-    IFS=$'\n' read -r -d '' -a i_RXs < <( ifconfig -a | grep "RX packets" | awk '{print $5}' | tr -d : && printf '\0' )  
-    IFS=$'\n' read -r -d '' -a f_TXs < <( ifconfig -a | grep "TX packets" | awk '{print $5}' | tr -d : && printf '\0' )
-    for ((i=1; i < $N; i++ ))
-    do
-        data[$i]="${interfaces[$i]} ${i_RXs[$i]} ${f_TXs[$i]}";
-        echo ${data[$i]};
-    done    
-    
+    while [ $looping -eq 1 ]; do 
 
-
-
-    # get wanted data
-    for ((i=1; i < $N; i++ ))
-    do  
-        # get innterface
-        interface=$(echo "${i_data[$i]}" | awk '{print $1;}');
-
-        # rx/tx = get rx/tx from array  |  Gap = rFinal - rInicial  |  rate  = Gap/#secs
-
-        # r
-        i_rx=$(echo "${i_data[$i]}" | awk '{print $2;}');
-        rx=$(echo "${data[$i]}" | awk '{print $2;}');
-        r_Gap=($(($rx-$i_rx)));
-        r_Gap=$((r_Gap/byte_div));
-        r_Rate=$(bc <<<"scale=1;$r_Gap/$sleep_time");
-
-        # t
-        i_tx=$(echo "${i_data[$i]}" | awk '{print $3;}');
-        tx=$(echo "${data[$i]}" | awk '{print $3;}');
-        t_Gap=($(($tx-$i_tx))); 
-        t_Gap=$((t_Gap/byte_div));
-        t_Rate=$(bc <<<"scale=1;$t_Gap/$sleep_time");
-
-        # return changed and added values to the array  
-        data[$i]="$interface $t_Gap $r_Gap $t_Rate $r_Rate";
+        # get inicial array and organize it in: i_data[] = interface_name rx tx
+        IFS=$'\n' read -r -d '' -a interfaces < <( ifconfig -a | grep ": " | awk '{print $1}' | tr -d : && printf '\0' )
+        IFS=$'\n' read -r -d '' -a i_RXs < <( ifconfig -a | grep "RX packets" | awk '{print $5}' | tr -d : && printf '\0' )  
+        IFS=$'\n' read -r -d '' -a f_TXs < <( ifconfig -a | grep "TX packets" | awk '{print $5}' | tr -d : && printf '\0' )
+        N=${#interfaces[@]};
+        for ((i=1; i < $N; i++ ))
+        do
+            i_data[$i]="${interfaces[$i]} ${i_RXs[$i]} ${f_TXs[$i]}";
+            echo ${i_data[$i]};
+        done    
         
+        # wait given seconds
+        sleep $sleep_time
+
+        # get final array and organize it in: i_data[] = interface_name rx tx
+        IFS=$'\n' read -r -d '' -a interfaces < <( ifconfig -a | grep ": " | awk '{print $1}' | tr -d : && printf '\0' )
+        IFS=$'\n' read -r -d '' -a i_RXs < <( ifconfig -a | grep "RX packets" | awk '{print $5}' | tr -d : && printf '\0' )  
+        IFS=$'\n' read -r -d '' -a f_TXs < <( ifconfig -a | grep "TX packets" | awk '{print $5}' | tr -d : && printf '\0' )
+        for ((i=1; i < $N; i++ ))
+        do
+            data[$i]="${interfaces[$i]} ${i_RXs[$i]} ${f_TXs[$i]}";
+            echo ${data[$i]};
+        done    
+        
+
+
+
+        # get wanted data
+        for ((i=1; i < $N; i++ ))
+        do  
+            # get innterface
+            interface=$(echo "${i_data[$i]}" | awk '{print $1;}');
+
+            # rx/tx = get rx/tx from array  |  Gap = rFinal - rInicial  |  rate  = Gap/#secs
+
+            # r
+            i_rx=$(echo "${i_data[$i]}" | awk '{print $2;}');
+            rx=$(echo "${data[$i]}" | awk '{print $2;}');
+            r_Gap=($(($rx-$i_rx)));
+            r_Gap=$((r_Gap/byte_div));
+            r_Rate=$(bc <<<"scale=1;$r_Gap/$sleep_time");
+
+            # t
+            i_tx=$(echo "${i_data[$i]}" | awk '{print $3;}');
+            tx=$(echo "${data[$i]}" | awk '{print $3;}');
+            t_Gap=($(($tx-$i_tx))); 
+            t_Gap=$((t_Gap/byte_div));
+            t_Rate=$(bc <<<"scale=1;$t_Gap/$sleep_time");
+
+            # return changed and added values to the array  
+            data[$i]="$interface $t_Gap $r_Gap $t_Rate $r_Rate";
+            
+        done
+    
+        # Sort
+        case $order in
+            r)
+                SortRx
+                ;;
+            t)
+                SortTx
+                ;;
+        esac
+        if [[ $reversed -eq 1 ]]
+        then 
+            Reverse
+        fi
+
+        if [ $N -gt $max ]; then
+            N=$(($max+1));
+        fi   
+
+        #print 
+        if [ $iter -eq 0 ]; then
+            printf "%9s %9s %9s %9s %9s\n" "NETIF" "TX" "RX" "TRATE" "RRATE"; # print
+        fi  
+        
+        for (( i=1; i < $N; i++ )); do
+            int=$(echo "${data[$i]}" | awk '{print $1;}');
+            tx=$(echo "${data[$i]}" | awk '{print $2;}');
+            rx=$(echo "${data[$i]}" | awk '{print $3;}');
+            t_rate=$(echo "${data[$i]}" | awk '{print $4;}');
+            r_rate=$(echo "${data[$i]}" | awk '{print $5;}');
+
+            printf "%9s %9s %9s %9s %9s\n" $int $tx $rx $t_rate $r_rate;
+        done
+
+        # define if its to continue looping or not
+        looping=$loop;
+        iter=$(($iter+1));
     done
- 
-    # Sort
-    case $order in
-        r)
-            SortRx
-            ;;
-        t)
-            SortTx
-            ;;
-    esac
-    if [[ $reversed -eq 1 ]]
-    then 
-        Reverse
-    fi
-
-    if [ $N -gt $max ]; then
-        N=$(($max+1));
-    fi   
-
-    #print 
-    printf "%9s %9s %9s %9s %9s\n" "NETIF" "TX" "RX" "TRATE" "RRATE"; # print
-    for (( i=1; i < $N; i++ )); do
-        int=$(echo "${data[$i]}" | awk '{print $1;}');
-        tx=$(echo "${data[$i]}" | awk '{print $2;}');
-        rx=$(echo "${data[$i]}" | awk '{print $3;}');
-        t_rate=$(echo "${data[$i]}" | awk '{print $4;}');
-        r_rate=$(echo "${data[$i]}" | awk '{print $5;}');
-
-        printf "%9s %9s %9s %9s %9s\n" $int $tx $rx $t_rate $r_rate;
-    done
-
 #
